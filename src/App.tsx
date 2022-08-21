4e9287d3375702c4ae4d88d11c1312c500959004
draxe <draxe1989@gmail.com>
2022-08-21 20:14:46 +0300

    start

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <h1 className='bg-amber-500 mx-auto w-[500px] h-[500px] flex justify-center items-center'>
+            RACT+REDUXTOOLKIT+TAILWINDCSS
+        </h1>
+    );
+}
+
+export default App;
